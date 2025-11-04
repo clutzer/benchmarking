7ad269e27f30c1a7d55b13f1831b525997eeef97
Christiaan Lutzer <clutzer@akamai.com>
2025-11-04 16:53:12 +0000

    Support GPUs inside of docker.

---
+++ gpu-nim/nvidia/step2_install_toolkit.sh
@@ -0,0 +1,8 @@
+sudo apt update
+
+export NVIDIA_CONTAINER_TOOLKIT_VERSION=1.18.0-1
+  sudo apt install -y \
+      nvidia-container-toolkit=${NVIDIA_CONTAINER_TOOLKIT_VERSION} \
+      nvidia-container-toolkit-base=${NVIDIA_CONTAINER_TOOLKIT_VERSION} \
+      libnvidia-container-tools=${NVIDIA_CONTAINER_TOOLKIT_VERSION} \
+      libnvidia-container1=${NVIDIA_CONTAINER_TOOLKIT_VERSION}
