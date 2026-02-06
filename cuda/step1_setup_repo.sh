03c0dd2bc249273acb0b0b6f2c8aa7f6fb494116
Christiaan Lutzer <clutzer@akamai.com>
2026-02-06 20:59:17 +0000

    Prototyping some multi-gpu performance testing

---
+++ cuda/step1_setup_repo.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+sudo apt install build-essential -y
+
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2404/x86_64/cuda-keyring_1.1-1_all.deb
+sudo dpkg -i cuda-keyring_1.1-1_all.deb
