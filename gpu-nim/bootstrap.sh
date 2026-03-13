05c3f2d60629409e50fce7d82d3a69a55a331e20
Christiaan Lutzer <clutzer@akamai.com>
2026-03-13 13:57:43 +0000

    Fixed spelling error

--- gpu-nim/bootstrap.sh
+++ gpu-nim/bootstrap.sh
@@ -5,7 +5,7 @@ VENV_DIR=python-venv
 sudo apt install python3-venv python-is-python3 make gcc nvtop
 
 if [ ! -d $VENV_DIR ]; then
-    echo "Creating '$VEND_DIR' Python virtual environment..."
+    echo "Creating '$VENV_DIR' Python virtual environment..."
     python -m venv $VENV_DIR
     source $VENV_DIR/bin/activate
     pip install --upgrade pip
